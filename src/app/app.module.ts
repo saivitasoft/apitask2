22e36b999d9689453a763613f81143e1247503ec
saivitasoft <saikumarvita@gmail.com>
2023-11-06 13:01:40 +0530

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Task1Component } from './task1/task1.component';
+import { Task2Component } from './task2/task2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Task1Component,
+    Task2Component
   ],
   imports: [
     BrowserModule,
