bebf3edac7b06f0f899c7d3e9fec6b14953e61fe
saivitasoft <saikumarvita@gmail.com>
2023-11-06 15:00:04 +0530

    http module add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Task1Component } from './task1/task1.component';
 import { Task2Component } from './task2/task2.component';
+import {  HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     Task1Component,
-    Task2Component
+    Task2Component,
+    
+    
+  
+  
+
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
